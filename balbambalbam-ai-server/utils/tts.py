ce4e37610eb7a2ec7ce3f26ba913c58128f287d2
seom-j <alswjd010619@gmail.com>
2024-10-07 17:22:36 +0900

    edit env

--- balbambalbam-ai-server/utils/tts.py
+++ balbambalbam-ai-server/utils/tts.py
@@ -2,13 +2,11 @@ import base64
 import httpx
 import os
 import sys
-from dotenv import load_dotenv
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-load_dotenv()
+from utils import load_config
 
 async def generate_voice(gender, age, text):
-
+    config = load_config()
     client_id = os.getenv('NAVER_TTS_API_ID')
     client_secret = os.getenv('NAVER_TTS_API_SECRET')
     
